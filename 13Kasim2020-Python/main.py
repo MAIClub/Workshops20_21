8404037d62998a2bcf31e92526c7885124ab2e5e
batuhanyenidunya <batuhanyndny@gmail.com>
2020-11-13 21:13:05 +0300

    Python review added

---
+++ 13Kasim2020-Python/main.py
@@ -0,0 +1,117 @@
+# kjasdkhajsgdkfhja 
+"""
+    [summary]
+    ASLJDHJKA
+    ASJDHKJAD
+"""
+# Intiger int
+# Float float
+# String str
+# Boolean bool 
+
+# toplamSayi cammelCase
+# toplam_sayi kebab-case
+# ToplamSayi 
+
+# x = 12;
+# kelime = 'kelime' 
+# is_cool = True;
+
+
+# a = 4+2
+# 5/2 = 2.5
+# 5//2 = 2
+
+
+# stringBes = "5"
+
+# print( type(stringBes) )
+
+# a = 8 % 3
+# print(a) 
+
+# elma_sayisi = 10
+# agac_sayisi = 123123
+
+# # print("elma agaci sayisi {agac}. ema sayisi {elma}".format(agac=agac_sayisi, elma=elma_sayisi  ))
+
+# print(f'elma agaci sayisi {agac_sayisi}')
+
+# def topla( deger1, deger2 ):
+#     """
+#         verilen 2 sayiyi toplar
+#     """
+#     toplam = deger1 + deger2
+
+#     return toplam
+
+
+# # # dondurulenDeger = 
+
+# print(topla(5,3))   
+
+
+
+#  a degeri eger 5 e esitse bana karesini yazdir 
+#  a degeri 10 a esitse bana guluck at
+#  degilse kendisini yazdir
+
+# a = 7
+# b = 11
+# if (a == 5):
+#     print(a**2)
+# if(b == 10):
+#     print(':*')
+# else:
+#     print(a)
+
+
+#  for dongus
+#  while dongusu
+
+# a = 5
+# while( a < 1000 ):
+#     a += 1 
+#     print(a)
+
+# print("Selam")
+
+# myA = [ 1, 3, 5, 7, 9, 11 ]
+
+# for i in myA:
+#     print("ELEMAN: ", i)
+
+# 1 den 100 e kadar olan sayilardan 3 bolunen ama 5e bolunemeyen sayilari yazdir
+# for i in range(1,100):
+#     if( i % 3 == 0 and i % 5 != 0):
+#         print(i)
+#     else:
+#         pass
+
+# kelime: kelime anlami
+
+# myDict = { "sahip" : "Emlakci", "m2" : 1500, "fiyat": 1500000}
+
+# print(myDict.values())
+
+
+
+# class Obje:
+
+#     def __init__(self, movable):
+#         self.canBeMoved = movable
+
+#     def tryToMove(self):
+#         """
+#        eger obje hareket ettirilebniliyosa ettiril yazar
+#         """
+#         if(self.canBeMoved):
+#             print("Ettirilir")
+#         else:
+#             print("ettirlemez")
+
+# kapi = Obje(True)
+# Dag = Obje(False)
+
+# kapi.tryToMove()
+# Dag.tryToMove()
